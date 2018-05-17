f3cff0053ec1a8c887ccfe7b73782773bfc8524e
Brooks Becton <broabect@ut.utm.edu>
2018-05-17 13:29:14 -0500

    Raise Page saving logic to App level

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 import SaveRecipeButton from "./SaveRecipeButton";
-
+import ScraperFactory from "./lib/scrapers/ScraperFactory";
 /** Wrapper for application */
 class App extends Component {
+  constructor() {
+    super();
+    this.scraperFactory = new ScraperFactory();
+  }
+
+  /**
+   * Creates a scraper for the user's current page and parses
+   * data from it.
+   */
+  scrapePageRecipe = async () => {
+    const scraper = await this.scraperFactory.create();
+    scraper.getHTML();
+  };
+
   render() {
     return (
       <div>
-        <SaveRecipeButton />
+        <SaveRecipeButton scrapePageRecipe={this.scrapePageRecipe} />
       </div>
     );
   }
--- src/SaveRecipeButton/index.js
+++ src/SaveRecipeButton/index.js
@@ -3,12 +3,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import ScraperFactory from "./../lib/scrapers/ScraperFactory";
-
 /** Saves the users current page content */
 class SaveRecipeButton extends Component {
   static propTypes = {
-    handleSave: PropTypes.func
+    scrapePageRecipe: PropTypes.func
   };
 
   /**
@@ -23,22 +21,6 @@ class SaveRecipeButton extends Component {
   }
 
   /**
-   * Captures the page source of the page the user's active page
-   * Uses {@link https://developer.chrome.com/extensions/pageCapture | pageCapture}
-   * @returns <Promise> - represents the html of the current page
-   */
-  capturePage = () => {
-    return new Promise((resolve, reject) => {
-      chrome.tabs.executeScript(
-        { code: "document.querySelector('body').innerHTML" },
-        ({ 0: html }) => {
-          resolve(html);
-        }
-      );
-    });
-  };
-
-  /**
    * Sets the state of 'saveError' to a supplied error message
    * @param {Error} err
    */
@@ -51,7 +33,7 @@ class SaveRecipeButton extends Component {
    * @async
    */
   saveRecipe = async () => {
-    let html = await this.capturePage();
+    this.props.scrapePageRecipe();
   };
   render() {
     return (
