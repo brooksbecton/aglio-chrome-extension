cd72a44d643f3909b2eb1479d280192346b2ee75
Brooks Becton <broabect@ut.utm.edu>
2018-05-18 16:24:29 -0500

    Implement AllRecipes Scraper

--- src/lib/scrapers/AllRecipesScraper/index.js
+++ src/lib/scrapers/AllRecipesScraper/index.js
@@ -5,29 +5,48 @@ import BaseScraper from "./../BaseScraper";
  * @extends BaseScraper
  */
 class AllRecipesScraper extends BaseScraper {
-  constructor() {
-    super();
+  /**
+   * Grabs and returns the directions for a recipe
+   * @returns An array of strings corresponding to the recipe's steps
+   */
+  getRecipe() {
+    const recipeNodeList = document.querySelectorAll(
+      ".recipe-directions__list li span"
+    );
+
+    const recipeSteps = Array.from(recipeNodeList);
+    return recipeSteps.map(({ innerHTML }) =>
+      innerHTML.replace("↵", "").trim()
+    );
   }
 
-  getRecipe() {
-    const recipeSteps = document.querySelectorAll(
-      ".recipe-directions__list li"
-    );
-    return recipeSteps.map(({ innerHTML }) => innerHTML);
-  }
   /**
    * Gets all ingredients from .list-ingredients-x
    * There can be more than one list
    */
   getIngredients() {
+    /**
+     * Queries DOM for a
+     * @param {Number} listNumber - the number of the list currently being sought
+     */
+    function getIngredientList(listNumber) {
+      const ingredientsNodeList = document.querySelectorAll(
+        `.list-ingredients-${listNumber} li span`
+      );
+      return Array.from(ingredientsNodeList);
+    }
+
     let allIngredients = [];
     let ingredientListCount = 1;
-    const ingredients = document.querySelectorAll(
-      `.list-ingredients-${ingredientListCount} li`
-    );
-    while (ingredients) {
-      allIngredients.push(ingredients.map(({ innerHTML }) => innerHTML));
+    let ingredients = getIngredientList(ingredientListCount);
+
+    while (ingredients.length > 0) {
+      allIngredients = [
+        ...allIngredients,
+        ...ingredients.map(({ innerHTML }) => innerHTML.trim())
+      ];
       ingredientListCount += 1;
+      ingredients = getIngredientList(ingredientListCount);
     }
     return allIngredients;
   }
---
+++ src/lib/scrapers/AllRecipesScraper/index.test.js
@@ -0,0 +1,46 @@
+import AllRecipesScraper from "./index";
+
+describe("AllRecipesScraper", () => {
+  it("pulls recipe steps", () => {
+    /**
+     * This HTML string follows the same structure as allrecipes page
+     * But it's recipe steps are "Add Salt, Add Pepper, and Bake"
+     */
+    document.body.innerHTML = `<ol class="list-numbers recipe-directions__list"itemprop=recipeInstructions><li class=step ng-class="{'finished': stepIsActive0}"ng-click="stepIsActive0 = !stepIsActive0"><span class=recipe-directions__list--item>Add Salt</span><li class=step ng-class="{'finished': stepIsActive1}"ng-click="stepIsActive1 = !stepIsActive1"><span class=recipe-directions__list--item>Add Pepper</span><li class=step ng-class="{'finished': stepIsActive2}"ng-click="stepIsActive2 = !stepIsActive2"><span class=recipe-directions__list--item>Bake</span></ol>`;
+
+    const testScraper = new AllRecipesScraper();
+    const steps = testScraper.getRecipe();
+
+    expect(steps).toEqual(["Add Salt", "Add Pepper", "Bake"]);
+  });
+
+  it("pulls ingredients from 1 list", () => {
+    /**
+     * This HTML string follows the same structure as allrecipes page
+     * But it's ingredients are "1 Peanut Butter, 1 Jelly, 1 Bread"
+     */
+    document.body.innerHTML = `<ul class="checklist dropdownwrapper list-ingredients-1"id=lst_ingredients_1 ng-hide=reloaded><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/3 cup plain bread crumbs"><input data-id=2112 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,2112) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=2112 data-nameid=2112 itemprop=ingredients>1 Peanut Butter</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/2 cup milk"><input data-id=16278 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,16278) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=16278 data-nameid=16278 itemprop=ingredients>1 Jelly</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="2 tablespoons olive oil"><input data-id=9725 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,9725) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=9725 data-nameid=9725 itemprop=ingredients>1 Bread</span></label><div class=pol-biu-container></div></ul>`;
+
+    const testScraper = new AllRecipesScraper();
+    const ingredients = testScraper.getIngredients();
+
+    expect(ingredients).toEqual(["1 Peanut Butter", "1 Jelly", "1 Bread"]);
+  });
+
+  it("pulls ingredients from more than 1 list", () => {
+    // ingredients are "1 Peanut Butter, 1 Jelly, 1 Bread, 1 Peanut Butter, 1 Jelly, 1 Bread"
+    document.body.innerHTML = `<ul class="checklist dropdownwrapper list-ingredients-1"id=lst_ingredients_1 ng-hide=reloaded><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/3 cup plain bread crumbs"><input data-id=2112 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,2112) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=2112 data-nameid=2112 itemprop=ingredients>1 Peanut Butter</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/2 cup milk"><input data-id=16278 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,16278) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=16278 data-nameid=16278 itemprop=ingredients>1 Jelly</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="2 tablespoons olive oil"><input data-id=9725 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,9725) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=9725 data-nameid=9725 itemprop=ingredients>1 Bread</span></label><div class=pol-biu-container></div></ul><ul class="checklist dropdownwrapper list-ingredients-1"id=lst_ingredients_1 ng-hide=reloaded><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/3 cup plain bread crumbs"><input data-id=2112 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,2112) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=2112 data-nameid=2112 itemprop=ingredients>1 Peanut Butter</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="1/2 cup milk"><input data-id=16278 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,16278) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=16278 data-nameid=16278 itemprop=ingredients>1 Jelly</span></label><div class=pol-biu-container></div><li class=checkList__line><label class=checkList__item ng-class="{true: 'checkList__item'}[true]"title="2 tablespoons olive oil"><input data-id=9725 data-role=none name=ingredientCheckbox ng-click=saveIngredient($event,9725) type=checkbox value=N> <span class="added recipe-ingred_txt"data-id=9725 data-nameid=9725 itemprop=ingredients>1 Bread</span></label><div class=pol-biu-container></div></ul>`;
+
+    const testScraper = new AllRecipesScraper();
+    const ingredients = testScraper.getIngredients();
+
+    expect(ingredients).toEqual([
+      "1 Peanut Butter",
+      "1 Jelly",
+      "1 Bread",
+      "1 Peanut Butter",
+      "1 Jelly",
+      "1 Bread"
+    ]);
+  });
+});
