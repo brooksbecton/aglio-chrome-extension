d578307cc245c35272fa3c42671f90f53e2f4e05
Brooks Becton <broabect@ut.utm.edu>
2018-05-24 16:24:25 -0500

    Add renderItem function and add more docs

--- src/components/RecipeDetails/index.js
+++ src/components/RecipeDetails/index.js
@@ -15,55 +15,54 @@ import "./index.css";
  */
 class RecipeDetails extends Component {
   /**
-   * Initilizes ingredients, recipe, and tempRecipe to empty sets/arrays in state
+   * Initilizes ingredients & recipe to empty sets in state
    */
   constructor() {
     super();
 
     this.state = {
       ingredients: new Set(),
-      recipe: new Set(),
-      tempRecipe: []
+      recipe: new Set()
     };
-  }
-
-  componentDidMount() {
-    this.setState({ tempRecipe: this.props.recipe.recipe });
-  }
-
-  /** Updates the components version of the recipe to matche the new recipe prop */
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.recipe !== prevProps.recipe) {
-      this.setState({ tempRecipe: this.props.recipe.recipe });
-    }
   }
 
   /**
    * Adds the instructions index to the indices of items being edited
    * @param {Number} instructionIndex
-   * @param {String} type - "recipe", "ingredients", or "meta"
+   * @param {String} itemType - "recipe", "ingredients", or "meta"
    */
-  editItem(instructionIndex, type) {
-    const t = new Set([instructionIndex, ...this.state[type]]);
+  openEditItem(instructionIndex, itemType) {
+    const t = new Set([instructionIndex, ...this.state[itemType]]);
     this.setState({
-      [type]: t
+      [itemType]: t
     });
   }
 
   /**
    * Removes the given index from the set of indices of items being edited
    * @param {Number} instructionIndex  - index of the item being edited
+   * @param {String} itemType - "recipe", "ingredients", or "meta"
    */
-  closeEdit(instructionIndex) {
-    let t = this.state.recipe;
+  closeEdit(instructionIndex, itemType) {
+    let t = this.state[itemType];
     t.delete(instructionIndex);
-    this.setState({ recipe: t });
+    this.setState({ [itemType]: t });
   }
 
-  updateItem = (newItemText, type, index) => {
+  /**
+   * Updates parent component with newItemText
+   * @param {String} newItemText
+   * @param {String} itemType - "recipe", "ingredients", or "meta"
+   * @param {Number} index
+   */
+  updateItem(newItemText, itemType, index) {
     this.props.updateRecipe({
       ...this.props.recipe,
-      [type]: this.state.tempRecipe.map((item, i) => {
+      /* 
+      Only give back the new text if it is the index matches 
+      the index of the item that we are editing 
+      */
+      [itemType]: this.props.recipe[itemType].map((item, i) => {
         if (i !== index) {
           return item;
         } else {
@@ -71,77 +70,66 @@ class RecipeDetails extends Component {
         }
       })
     });
-  };
+  }
+
+  /**
+   * Displays UI for item information and editing.
+   * @param {Array} itemSet - Array of strings containing thing like recipe steps or ingredient list
+   * @param {String} itemType - "recipe", "ingredients", or "meta"
+   */
+  renderItem(itemSet, itemType) {
+    return (
+      <ListGroup>
+        {itemSet.map((step, i) => (
+          <ListGroupItem key={i}>
+            {this.state[itemType].has(i) ? (
+              <div className="itemContainer">
+                <textarea
+                  className="itemText"
+                  type="text"
+                  value={step}
+                  onChange={e => this.updateItem(e.target.value, itemType, i)}
+                  wrap="hard"
+                  rows="8"
+                />
+                <Button
+                  onClick={() => this.closeEdit(i, itemType)}
+                  bsStyle="primary"
+                  className={["button", "crudButton"]}
+                >
+                  <Glyphicon glyph="ok" />
+                </Button>
+              </div>
+            ) : (
+              <div className="itemContainer">
+                <span className="itemText">{step}</span>
+                <Button
+                  onClick={() => this.openEditItem(i, itemType)}
+                  bsStyle="info"
+                  className={["editItem", "button", "crudButton"]}
+                >
+                  <Glyphicon glyph="pencil" />
+                </Button>
+                <Button bsStyle="danger" className={["button", "crudButton"]}>
+                  <Glyphicon glyph="trash" />
+                </Button>
+              </div>
+            )}
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    );
+  }
 
   render() {
-    const ingredients = this.props.recipe.ingredients;
     return (
       <div>
-        <Tabs defaultActiveKey="Recipe">
+        <Tabs defaultActiveKey="Recipe" id="Recipe Details">
           <Tab eventKey="Recipe" title="Recipe">
-            <ListGroup>
-              {this.state.tempRecipe.map((step, i) => (
-                <ListGroupItem>
-                  {this.state.recipe.has(i) ? (
-                    <div className="itemContainer">
-                      <textarea
-                        className="recipeText"
-                        type="text"
-                        value={step}
-                        onChange={e =>
-                          this.updateItem(e.target.value, "recipe", i)
-                        }
-                        wrap="hard"
-                        rows="8"
-                      />
-                      <Button
-                        onClick={() => this.closeEdit(i)}
-                        bsStyle="primary"
-                        className={["button", "crudButton"]}
-                      >
-                        <Glyphicon glyph="ok" />
-                      </Button>
-                    </div>
-                  ) : (
-                    <div className="itemContainer">
-                      <span className="recipeText">{step}</span>
-                      <Button
-                        onClick={() => this.editItem(i, "recipe")}
-                        bsStyle="info"
-                        className={["button", "crudButton"]}
-                      >
-                        <Glyphicon glyph="pencil" />
-                      </Button>
-                      <Button
-                        bsStyle="danger"
-                        className={["button", "crudButton"]}
-                      >
-                        <Glyphicon glyph="trash" />
-                      </Button>
-                    </div>
-                  )}
-                </ListGroupItem>
-              ))}
-            </ListGroup>
+            {this.renderItem(this.props.recipe.recipe, "recipe")}
           </Tab>
-          <Tab eventKey="Ingredients" title="Ingredients">
-            <ListGroup>
-              {ingredients.map((ingredient, i) => (
-                <ListGroupItem className="itemContainer">
-                  <span className="recipeText">{ingredient}</span>{" "}
-                  <Button
-                    onClick={() => this.editItem(i, "ingredients")}
-                    bsStyle="info"
-                    className={["button", "crudButton"]}
-                  >
-                    <Glyphicon glyph="pencil" />
-                  </Button>
-                  <Button bsStyle="danger" className={["button", "crudButton"]}>
-                    <Glyphicon glyph="trash" />
-                  </Button>
-                </ListGroupItem>
-              ))}
-            </ListGroup>
+          <Tab eventKey="ingredients" title="Ingredients">
+            {this.renderItem(this.props.recipe.ingredients, "ingredients")}
           </Tab>
         </Tabs>
       </div>
